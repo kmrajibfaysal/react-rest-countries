e956a879716c371c553e196e0072f9a9c5b815cd
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-16 21:00:38 +0600

    added loadCountries component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import './App.css';
+
 function App() {
     return (
         <div>
             <h1>Hello world!</h1>
+            <LoadCountries />
+        </div>
+    );
+}
+
+function LoadCountries() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then((response) => response.json())
+            .then((data) => setCountries(data));
+    }, []);
+    return (
+        <div>
+            <h1>Visiting Every country of the World!!</h1>
+            <h3>Available countries: {countries.length}</h3>
         </div>
     );
 }
